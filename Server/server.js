a8843f2f033294b50b57b91eaba24dd2b1100e84
Edward Roh <edroh90@gmail.com>
2018-02-20 20:28:56 -0800

    created server in server.js. changed package.json for npm start (nodemon server/server.js). commented out sequalize page until mysql server is fixed!

--- Server/server.js
+++ Server/server.js
@@ -2,9 +2,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const path = require('path');
-const sqlServer = require('./db/index');
+const mysql = require('mysql');
+// const sqlServer = require('./db/index');
 
 let app = express();
+
+
+// MYSQL DB CONNECTION
+// mysql connection works, but we get an error (Error: connect ECONNREFUSED 127.0.0.1:3306)
+let connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'eddie',
+  password: 'eddie',
+  database: 'instaDB'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.log(err);
+  }
+  console.log('Connected to InstaDB');
+});
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));

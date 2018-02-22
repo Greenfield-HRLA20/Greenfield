243aaaabe77bfec4c60ef585d4f87ebd44b74242
Edward Roh <edroh90@gmail.com>
2018-02-21 17:22:15 -0800

    working on sequelize models and queries

--- Server/server.js
+++ Server/server.js
@@ -2,27 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const path = require('path');
-const mysql = require('mysql');
-const router = require('./router.js');
-// const sqlServer = require('./db/index');
+const sqlServer = require('./db/index');
 
 let app = express();
-
-// MYSQL DB CONNECTION
-let connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  // password: '2/)dvk940XrY',
-  password: 'password',
-  database: ''
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.log(err);
-  }
-  console.log('Connected to InstaDB');
-});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));

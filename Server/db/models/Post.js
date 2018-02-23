bcd63e0ef282bafb6ec5886b83957af7c4639f5e
Jason <jsato03@gmail.com>
2018-02-23 10:12:28 -0800

    Modified Post model file

--- Server/db/models/Post.js
+++ Server/db/models/Post.js
@@ -1,11 +1,20 @@
 const connection = require('../index.js');
 const Sequelize = require('sequelize');
-const User = require('./User.js');
+// const User = require('./User.js');
 
 const Post = connection.define('post', {
   caption: Sequelize.STRING,
   url: Sequelize.STRING,
   createdAt: Sequelize.DATE,
+});
+
+const User = connection.define('user', {
+  firstName: Sequelize.STRING,
+  lastName: Sequelize.STRING,
+  handle: {
+    type: Sequelize.STRING,
+    unique: true
+  },
 });
 
 // Post.belongsTo(User, { foreignKey: { allowNull: false }, onDelete: 'CASCADE' });

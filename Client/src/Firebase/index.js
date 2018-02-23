339abd68d49f8c17494ffbfc08e5398b02ad755b
Justin Dai <justingdai@gmail.com>
2018-02-22 16:04:40 -0800

    Reworked Main and App components for redux

--- Client/src/Firebase/index.js
+++ Client/src/Firebase/index.js
@@ -17,7 +17,7 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
 var uiConfig = {
   callbacks: {
     signInSuccess: function(currentUser, credential) {
-      return true;
+      return false;
     },
     uiShown: function() {
       

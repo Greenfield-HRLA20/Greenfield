a5476cfaf538793f1d313daaa264356fb076cf36
Edward Huang <ehuange@gmail.com>
2018-03-05 15:47:01 -0800

    Try to deploy

--- Server/db/index.js
+++ Server/db/index.js
@@ -5,7 +5,8 @@ const config = require('../../database.config');
 const connection = new Sequelize('instadb', config.user, config.password, {
   host: config.ip,
   dialect: 'mysql',
-  port: 3306
+  port: 3306,
+  connectTimeout: 30000
 });
 
 module.exports = connection;
